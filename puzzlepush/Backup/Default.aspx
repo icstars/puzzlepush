c7e51b216b19feadc296373788c7b89c222fca44
RigelM <rigel.marcinik@gmail.com>
2013-06-26 11:08:28 -0500

    Added connection to DB and convert to JSON

---
+++ puzzlepush/Backup/Default.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="puzzlepush._Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        Hello World
+    </h2>
+    <p>
+        From vs.net to github to azure to a phone
+    </p>
+</asp:Content>
---
+++ puzzlepush/hw.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+namespace puzzlepush
+{
+    public partial class hw : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string ConnectionString;
+            ConnectionString = "Password=!31497Oo;User ID=dbdev;Initial Catalog=HW;Integrated Security=True;Trusted_Connection=No;Data Source=ics-c28-02.cloudapp.net";
+
+            SqlConnection objConn = new SqlConnection(ConnectionString);
+
+            SqlDataAdapter MyAdapter = new SqlDataAdapter("dbo.hello_world", objConn);
+
+
+
+            DataSet ds = new DataSet();
+            try
+            {
+                objConn.Open();
+                MyAdapter.Fill(ds, "hw");
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+
+            // DataRow drow = ds.Tables["hw"].Rows[0];
+            // string retval = drow["username"].ToString();
+
+            string json = JsonConvert.SerializeObject(ds);
+            Response.Write(json);
+        }
+    }
+}
