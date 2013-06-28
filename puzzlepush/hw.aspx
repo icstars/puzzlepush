706f5f53b60e72d5faa4acf08ca19f2771ddec32
RigelM <rigel.marcinik@gmail.com>
2013-06-28 10:59:24 -0500

    Added Comments for clarity
    Also added a jquery test call

--- puzzlepush/hw.aspx
+++ puzzlepush/hw.aspx
@@ -2,15 +2,34 @@
 
 <!DOCTYPE html>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
+<html>
+<head>
+    
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
+    
+    <title>Jquery calling C#</title>
+
 </head>
 <body>
-    <form id="form1" runat="server">
-    <div>
+
+    <div id="Result">Why wont this work</div>
     
-    </div>
-    </form>
+    <script>
+        $(document).ready(function () {
+            $("#Result").click(function () {
+                $.ajax({
+                    type: "POST",
+                    url: "hw.aspx/getjson",
+                    data: "{}",
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    success: function (msg) {
+                        $("#Result").text(msg.d);
+                    }
+                });
+            });
+        });    
+    </script>
+    
 </body>
 </html>
--- puzzlepush/hw.aspx.cs
+++ puzzlepush/hw.aspx.cs
@@ -2,36 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 using System.Data;
 using System.Data.SqlClient;
 using Newtonsoft.Json;
+
 namespace puzzlepush
 {
     public partial class hw : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // this was our first test to see if json was being serialized, the jquery test has replaced this need.
+           // string j = getjson();
+           // Response.Write(j);
+        }
+
+        [WebMethod]
+        public static string getjson()
+        {
+            // define a connection to the database 
             string ConnectionString;
             ConnectionString = "Password=!31497Oo;User ID=dbdev;Initial Catalog=HW;Integrated Security=True;Trusted_Connection=No;Data Source=ics-c28-02.cloudapp.net";
             SqlConnection objConn = new SqlConnection(ConnectionString);
+
+            //run an sql query and create a dataset to store the result
             SqlDataAdapter MyAdapter = new SqlDataAdapter("selecthelloworldbyid'1'", objConn);
             DataSet ds = new DataSet();
 
             try
             {
+                //open the connection to the database
+                //fill the dataset with the results from the sql query and name the table 'hw'
                 objConn.Open();
                 MyAdapter.Fill(ds, "hw");
             }
+
             catch (Exception ex)
             {
-                Response.Write(ex.Message);
+                //write any error messages
+            //    Response.Write(ex.Message);
             }
 
+            //Using Newtonsoft's JSON.NET, convert our dataset object from C# to JSON (JavaScript Object Notation)
             string json = JsonConvert.SerializeObject(ds);
-            Response.Write(json);
+            
+            return json;
+
             
         }
     }
