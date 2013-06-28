086874fd482f59debb7f575f61fa385097d59615
lsampson74 <lsampson74@gmail.com>
2013-06-28 18:43:35 -0500

    changed from cross domain url to use webmethod

--- puzzlepush/Scripts/test.js
+++ puzzlepush/Scripts/test.js
@@ -2,7 +2,7 @@ function displayMessageCall(){
 
     $.ajax({
        //type:"GET",
-       url:"http://ics-c28-02.cloudapp.net:8080",
+       url:"hw.aspx/getjson",
         //url: "http://icstarstestserv.appspot.com/icstarstest",
         //data: 'HelloWorld',
         // contentType:"application/json",
