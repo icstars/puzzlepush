8ac575dc081403186d747670cf108c0a9d03eeb4
icstars <appldev@icstars.org>
2013-06-28 21:20:35 -0500

    fixed the call to be type post and proper retrieval of msg.d

--- puzzlepush/scripts/test.js
+++ puzzlepush/scripts/test.js
@@ -1,14 +1,13 @@
 function displayMessageCall(){
 
     $.ajax({
-       //type:"GET",
-       url:"hw.aspx/getjson",
-        //url: "http://icstarstestserv.appspot.com/icstarstest",
-        //data: 'HelloWorld',
-        // contentType:"application/json",
-         dataType:"json",
+        type: "POST",
+        url: "hw.aspx/getjson",
+        data: "{}",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
         success: function(msg){
-        $("#HelloWorldString").text(msg);
+        $("#HelloWorldString").text(msg.d);
         alert(msg);
         }
     });
