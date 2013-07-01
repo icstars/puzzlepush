2eceaf79f25a8d43be39320035e89f4c703b4cd8
RigelM <rigel.marcinik@gmail.com>
2013-07-01 18:30:54 -0500

    Added RandomArray, arrayer() method
    Creates a 2D array, and fills it with random elements. (Elements will be
    tiles for the gameboard)

---
+++ puzzlepush/RandomArray/RandomArray/makearray.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+
+namespace RandomArray
+{
+    public partial class makearray : System.Web.UI.Page
+    {
+
+        //List<string> mylist = new List<string>();
+        string[,] mylist = new string[5, 5];
+        Random number = new Random();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            arrayer();
+            string json = JsonConvert.SerializeObject(mylist);
+            Response.Write(json);
+        }
+
+        public void arrayer()
+        {
+            List<string> names = new List<string>() { "Here", "Are", "Some", "Random", "Strings", "Six" };
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    string n = names[randomnumber()];
+                    //mylist.Add(n);
+                    mylist[i, j] = n;
+                };
+            }
+
+        }
+
+        public int randomnumber()
+        {
+            int rn = number.Next(0, 6);
+            return rn;
+        }
+
+    }
+}
