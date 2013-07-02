a8d390386afae3da5f4fa33340458ad7e7d35f31
RigelM <rigel.marcinik@gmail.com>
2013-07-02 13:52:37 -0500

    Added WebMethod getarray()
    Added comments for clarity

--- puzzlepush/randomarray.aspx.cs
+++ puzzlepush/randomarray.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -13,37 +14,94 @@ namespace puzzlepush
 {
     public partial class randomarray : System.Web.UI.Page
     {
-        //List<string> mylist = new List<string>();
-        string[,] mylist = new string[5, 5];
-        Random number = new Random();
+        static Random number = new Random();
+        static DataSet ds = new DataSet();
+        static DataTable dt = new DataTable();
+
+
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            arrayer();
-            string json = JsonConvert.SerializeObject(mylist);
-            Response.Write(json);
+            //connect();
+            
+
+            //string json = JsonConvert.SerializeObject(arrayer());
+            //Response.Write(json);
         }
 
-        public void arrayer()
+        [WebMethod]
+        public static string getarray()
         {
-            List<string> names = new List<string>() { "Here", "Are", "Some", "Random", "Strings", "Six" };
+            //connect and use arrayer, return as JSON
+            connect();
+
+            string json = JsonConvert.SerializeObject(arrayer());
+            return json;
+        }
+
+        public static Array arrayer()
+        {
+            //makes a 2D array and adds each element from the DataTable list to it
+            string[,] myarray = new string[5, 5];
+            List<String> names = makelist();
 
             for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 5; j++)
                 {
-                    string n = names[randomnumber()];
-                    //mylist.Add(n);
-                    mylist[i, j] = n;
+                    String n = names[randomnumber()];
+
+                    myarray[i, j] = n;
                 };
             }
 
+            return myarray;
         }
 
-        public int randomnumber()
+        public static List<String> makelist()
         {
-            int rn = number.Next(0, 6);
+            //takes the DataTable and casts it to a String list
+            List<String> stringlist = new List<String>();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                string s = (string)dt.Rows[i]["image_name"];
+                stringlist.Add(s);
+
+            }
+            return stringlist;
+        }
+
+        public static int randomnumber()
+        {
+            //Return a random number between 0 and 4
+            int rn = number.Next(0, 5);
             return rn;
+        }
+
+        public static void connect()
+        {
+            try
+            {
+                string ConnectionString = "Password=!31497Oo;User ID=dbdev;Initial Catalog=pp;Integrated Security=True;Trusted_Connection=No;Data Source=ics-c28-02.cloudapp.net";
+                SqlConnection objConn = new SqlConnection(ConnectionString);
+
+                //run an sql query and create a dataset to store the result
+                SqlDataAdapter MyAdapter = new SqlDataAdapter("select image_name from images", objConn);
+
+                //open the connection to the database
+                //fill the dataset with the results from the sql query and name the table 'hw'
+                objConn.Open();
+                MyAdapter.Fill(ds, "ra");
+                dt = ds.Tables["ra"];
+                objConn.Close();
+            }
+
+            catch (Exception ex)
+            {
+                //write any error messages
+                Console.WriteLine(ex.Message);
+
+            }
         }
     }
 }
