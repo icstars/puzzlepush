b661727739461a4a292cb6f3ccd7951c8220e264
RigelM <rigel.marcinik@gmail.com>
2013-07-08 19:09:56 -0500

    Splashpage.aspx returns an IP address
    The goal is to accept a date from jQuery and generate and IP and store
    both in the database

--- puzzlepush/splashpage.aspx.designer.cs
+++ puzzlepush/splashpage.aspx.designer.cs
@@ -10,6 +10,15 @@
 namespace puzzlepush {
     
     
-    public partial class splashpage {
+    public partial class sp1 {
+        
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
