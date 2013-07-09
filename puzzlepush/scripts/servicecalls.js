b2d6f4201f2989fb0740b23916de6a074c6bbd1e
Nicolette Chambers <nchambers@icstars.org>
2013-07-08 19:40:50 -0500

    servicecalls.js calling play.aspx/recordstart

---
+++ puzzlepush/scripts/servicecalls.js
@@ -0,0 +1,13 @@
+ 
+   function getplaygame(){
+	var thedate = new Date();
+	$.ajax(){
+		type:"Post", 
+		url:"play.aspx/recordstart",
+		data:{date: thedate},
+		contentType:"application/json", 
+		success:function(msg){
+     	console.log(msg.d);
+	}
+}
+
