403205f27cd324351e5b149523d4bbd7a7abdb00
RigelM <rigel.marcinik@gmail.com>
2013-07-19 15:52:06 -0500

    Board class added, arrayboard, playerid, Id attributes

---
+++ puzzlepush/Board.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+
+namespace puzzlepush
+{
+    [DataContract]
+    public class Board
+    {
+        private string[,] array;
+
+        [DataMember]
+        public string[,] arrayboard
+        {
+
+            get { return array; }
+            set { array = value; }
+        }
+
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public int playerid
+        {
+            get;
+            set;
+        }
+    }
+}
