c684503210f5c3a91bf48a682ce951ca2f4a7121
RigelM <rigel.marcinik@gmail.com>
2013-07-19 18:01:59 -0500

    added Score.cs

---
+++ puzzlepush/Score.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace puzzlepush
+{
+    public class Score
+    {
+        private string privatescore;
+
+        public string Scores
+        {
+            get { return privatescore; }
+            set { privatescore = value; }
+        }
+
+        public int scoreid
+        {
+            get;
+            set;
+        }
+    }
+}
